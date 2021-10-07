f377e077ecd398705e0d3c4071c6bbed294e118f
tolgakinventuna <tolga.karanlikoglu@inventuna.com>
2021-10-07 16:54:57 +0300

    Parallax sahnesi

---
+++ Assets/Scripts/GUIParallaxHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIParallaxHandler : MonoBehaviour
+{
+    public GUIParallaxLayer[] Layers;
+    public float LimitLeft = -1000;
+    public float LimitRight = 1000;
+
+    void Start()
+    {
+        // if you need to make any initialization, make it here
+    }
+
+    void Update()
+    {
+        // Handle input here
+    }
+}
---
+++ Assets/Scripts/GUIParallaxLayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIParallaxLayer : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
