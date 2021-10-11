338bedda5f15e8bd86d4368991d2132522b9ce1e
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2021-10-11 10:06:46 +0300

    Görev #8002 için değişiklikler

---
+++ Assets/Scripts/UI/IntensityChanger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntensityChanger : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float Intensity = 1;
+
+    float lastIntensity;
+
+    void Start()
+    {
+        SetIntensity();
+    }
+
+    private void SetIntensity()
+    {
+        // MERT GÖREV BAŞLANGICI
+
+        // MERT GÖREV BİTİŞİ
+
+        lastIntensity = Intensity;
+    }
+
+    void Update()
+    {
+        if(lastIntensity != Intensity)
+        {
+            SetIntensity();
+        }
+    }
+}
