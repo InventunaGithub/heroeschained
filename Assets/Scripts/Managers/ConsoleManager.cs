e5b83b160a7fc9035aef683b2a94fa19a58b8daa
Mert Karavural <mert.karavural@inventuna.com>
2021-10-16 23:38:45 +0300

    Commands added with space in them .

--- Assets/Scripts/Managers/ConsoleManager.cs
+++ Assets/Scripts/Managers/ConsoleManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using CommandTerminal;
 using System.IO;
 
@@ -16,7 +17,106 @@ public class ConsoleManager : MonoBehaviour
         Terminal.Log("Version 0");
     }
 
-    [RegisterCommand(Name = "Variables.Add", Help = "Adds a new variable to the variables (variables.addnew VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
+    [RegisterCommand(Name = "Variables", Help = "Variables command", MinArgCount = 1, MaxArgCount = 3)]
+    static void CommandVariables(CommandArg[] args)
+    {
+        string command = args[0].String;
+        if(command.ToLower() == "add")
+        {
+            if(args.Length == 3)
+            {
+                string variableName = args[1].String;
+                object value = args[2];
+                VariableManager.Instance.Add(variableName, value);
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+                Terminal.Log(variableName + " added with value :  " + value.ToString());
+            }
+            else
+            {
+                throw new Exception("not enough arguments.");
+            }
+        }
+        else if (command.ToLower() == "set")
+        {
+            if (args.Length == 3)
+            {
+                string variableName = args[1].String;
+                object value = args[2];
+                VariableManager.Instance.Set(variableName, value);
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+                Terminal.Log(variableName + " added with value :  " + value.ToString());
+            }
+            else
+            {
+                throw new Exception("not enough arguments.");
+            }
+        }
+        else if (command.ToLower() == "get")
+        {
+            if (args.Length == 2)
+            {
+                string variableName = args[1].String;
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+
+                Terminal.Log("Value : " + VariableManager.Instance.Get(variableName).ToString());
+            }
+            else
+            {
+                if(args.Length == 3)
+                {
+                    throw new Exception("Too Many arguments.");
+                }
+                if (args.Length == 1)
+                {
+                    throw new Exception("not enough arguments.");
+                }
+            }
+        }
+        else if (command.ToLower() == "list")
+        {
+            if (args.Length == 1)
+            {
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+                foreach (string item in VariableManager.Instance.ListAll())
+                {
+                    Terminal.Log(item);
+                }
+            }
+            else
+            {
+                throw new Exception("Too Many arguments.");
+            }
+        }
+        else if (command.ToLower() == "dump")
+        {
+            if (args.Length == 1)
+            {
+                string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/dump.txt";
+                string allVariables = "";
+                foreach (string item in VariableManager.Instance.ListAll())
+                {
+                    allVariables += item + " \n";
+                }
+                File.WriteAllText(path, allVariables);
+
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+
+                Terminal.Log("Dump.txt created at " + path);
+            }
+            else
+            {
+                throw new Exception("Too Many arguments.");
+            }
+        }
+        else
+        {
+            throw new Exception("Command does not exist");
+        }
+
+
+    }
+
+    [RegisterCommand(Name = "Variables.Add", Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
     static void CommandVariablesAdd(CommandArg[] args)
     {
         string variableName = args[0].String;
@@ -25,7 +125,7 @@ public class ConsoleManager : MonoBehaviour
 
         if (Terminal.IssuedError) return; // Error will be handled by Terminal
 
-        Terminal.Log(variableName + " Added with the value " + value.ToString());
+        Terminal.Log(variableName + " added with value  " + value.ToString());
     }
 
     [RegisterCommand(Name = "Variables.Set" , Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
