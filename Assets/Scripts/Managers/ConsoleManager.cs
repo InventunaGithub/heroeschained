650e9c397aff54c1cbf49fde6917c7ea9d5fe732
Mert Karavural <mert.karavural@inventuna.com>
2021-10-18 10:52:13 +0300

    İmport and reset method added to command console
    İmport and reset functions now can be sued , also help page is now with spaces

--- Assets/Scripts/Managers/ConsoleManager.cs
+++ Assets/Scripts/Managers/ConsoleManager.cs
@@ -97,11 +97,55 @@ public class ConsoleManager : MonoBehaviour
                 {
                     allVariables += item + " \n";
                 }
+
                 File.WriteAllText(path, allVariables);
 
                 if (Terminal.IssuedError) return; // Error will be handled by Terminal
 
                 Terminal.Log("Dump.txt created at " + path);
+            }
+            else
+            {
+                throw new Exception("Too Many arguments.");
+            }
+        }
+        else if (command.ToLower() == "reset")
+        {
+            if (args.Length == 1)
+            {
+                VariableManager.Instance.Reset();
+
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+
+                Terminal.Log("All variables deleted. ");
+            }
+            else
+            {
+                throw new Exception("Too Many arguments.");
+            }
+        }
+        else if (command.ToLower() == "import")
+        {
+            if (args.Length == 1)
+            {
+                string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/dump.txt";
+                if(File.Exists(path))
+                {
+                    string[] Lines = File.ReadAllLines(path);
+
+                    foreach (string Line in Lines)
+                    {
+                        VariableManager.Instance.Add(Line.Split('-')[0].Trim(), Line.Split('-')[1].Trim());
+                    }
+                }
+                else
+                {
+                    throw new Exception("Dump.txt does not exist.");
+                }
+
+                if (Terminal.IssuedError) return; // Error will be handled by Terminal
+
+                Terminal.Log("Dump.txt read at " + path);
             }
             else
             {
@@ -116,67 +160,48 @@ public class ConsoleManager : MonoBehaviour
 
     }
 
-    [RegisterCommand(Name = "Variables.Add", Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
+    [RegisterCommand(Name = "Variables Add", Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
     static void CommandVariablesAdd(CommandArg[] args)
-    {
-        string variableName = args[0].String;
-        object value = args[1];
-        VariableManager.Instance.Add(variableName, value);
-
-        if (Terminal.IssuedError) return; // Error will be handled by Terminal
-
-        Terminal.Log(variableName + " added with value  " + value.ToString());
+    { 
+        //This is just for the help page
     }
 
-    [RegisterCommand(Name = "Variables.Set" , Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
+    [RegisterCommand(Name = "Variables Set" , Help = "Sets a variable to the given value (variables.set VarName Value)", MinArgCount = 2, MaxArgCount = 2)]
     static void CommandVariablesSet(CommandArg[] args)
     {
-        string variableName = args[0].String;
-        object value = args[1];
-        VariableManager.Instance.Set(variableName, value);
-
-        if (Terminal.IssuedError) return; // Error will be handled by Terminal
-
-        Terminal.Log(variableName + " set to  " + value.ToString());
+        //This is just for the help page
     }
 
-    [RegisterCommand(Name = "Variables.Get", Help = "Shows given variable", MinArgCount = 1 , MaxArgCount = 1)]
+    [RegisterCommand(Name = "Variables Get", Help = "Shows given variable", MinArgCount = 1 , MaxArgCount = 1)]
     static void CommandVariablesGet(CommandArg[] args)
     {
-        string variableName = args[0].String;
-        
-
-        if (Terminal.IssuedError) return; // Error will be handled by Terminal
-
-        Terminal.Log("Value : " + VariableManager.Instance.Get(variableName).ToString());
+        //This is just for the help page
     }
 
-    [RegisterCommand(Name = "Variables.List", Help = "Shows all variables")]
+    [RegisterCommand(Name = "Variables List", Help = "Shows all variables")]
     static void CommandVariablesList(CommandArg[] args)
     {
-        if (Terminal.IssuedError) return; // Error will be handled by Terminal
-
-        foreach (string item in VariableManager.Instance.ListAll())
-        {
-            Terminal.Log(item);
-        }
+        //This is just for the help page
     }
 
-    [RegisterCommand(Name = "Variables.dump", Help = "All variables dumped into variables.txt")]
+    [RegisterCommand(Name = "Variables Dump", Help = "All variables dumped into variables.txt")]
     static void CommandVariablesDump(CommandArg[] args)
     {
-        string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/dump.txt";
-        string allVariables = "";
-        foreach (string item in VariableManager.Instance.ListAll())
-        {
-            allVariables += item + " \n";
-        }
-        File.WriteAllText(path, allVariables);
-
-        if (Terminal.IssuedError) return; // Error will be handled by Terminal
-
-        Terminal.Log("Dump.txt created at " + path);
+        //This is just for the help page
     }
+
+    [RegisterCommand(Name = "Variables Reset", Help = "Clears all variables")]
+    static void CommandVariablesReset(CommandArg[] args)
+    {
+        //This is just for the help page
+    }
+
+    [RegisterCommand(Name = "Variables Import", Help = "Reads from the Dump file ( resets all local variables created )")]
+    static void CommandVariablesImport(CommandArg[] args)
+    {
+        //This is just for the help page
+    }
+
 
     [RegisterCommand(Name = "Pause", Help = "Pauses the game")]
     static void CommandPause(CommandArg[] args)
