e4d7b8b5afc98131b3bbc8408c88dd8429c528e1
Deniz Afşar <deniz.afsar@inventuna.com>
2021-11-02 10:32:55 +0300

    import anti-cheat system on project

---
+++ Assets/Scripts/TradeScripts/AuctionManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AuctionManager : MonoBehaviour
+{
+    
+}
+
+public class Bid
+{
+    ObstructiveInt gold;
+    string owner;
+    ObstructiveInt reserved1;
+    ObstructiveInt reserved2;
+    ObstructiveInt reserved3;
+    float reserved4;
+    string reserved5;
+    DateTime givenAt;
+
+    public Bid()
+    {
+
+    }
+}
